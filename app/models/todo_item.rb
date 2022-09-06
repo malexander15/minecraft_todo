02af8d3f09e6ece38ea2fb0d5b08e6edcc3a4965
malexander15 <creepinjezus15@gmail.com>
2022-09-06 01:16:57 -0400

    Adding some styling and data for the todo item form

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,4 @@
 class TodoItem < ApplicationRecord
+  LIST_OF_ALL_MINECRAFT_ITEMS = ["Acacia Leaves", "Acacia Log", "Acacia Planks", "Acacia Sapling", "Acacia Stairs", "Acacia tree", "Acacia Slab"]
   belongs_to :todo_list
 end
