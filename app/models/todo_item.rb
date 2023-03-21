bb344edb83801b6c99278176c5c606289873db8c
malexander15 <creepinjezus15@gmail.com>
2023-03-21 13:17:10 -0400

    Getting rid of unnecessary stuff

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,3 @@
 class TodoItem < ApplicationRecord
-  LIST_OF_ALL_MINECRAFT_ITEMS = ["Acacia Leaves", "Acacia Log", "Acacia Planks", "Acacia Sapling", "Acacia Stairs", "Acacia tree", "Acacia Slab"]
   belongs_to :todo_list
 end
