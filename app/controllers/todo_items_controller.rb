f2cd07191b4663a54c6c28cb233742c28bcc6bda
malexander15 <creepinjezus15@gmail.com>
2023-04-01 13:24:05 -0400

    added back bootstrap styling because iI can't get any other frontend framework work. Added JSON to serialize the data from minecraft_variants.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,11 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
 	before_action :set_todo_item, except: [:create]
+	
+	def index
+		@minecraft_materials = MinecraftItems.all_variants
+		render json: @minecraft_materials.to_json
+	end
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
